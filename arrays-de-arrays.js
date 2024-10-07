68297c14bb08ba49a71a2812b9c4fda4d8110f3b
Phillipe Rodrigues <phillipeamodeus@outlook.com>
2024-10-07 19:38:49 -0300

    Array de Arrays e Atualizando listas

---
+++ arrays-de-arrays.js
@@ -0,0 +1,7 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const media = [10, 8, 7.5, 9 ];
+
+const lista = [alunos, media]
+
+console.log(`A aluna da posição 1 é ${lista[0][1]}.
+E a nota dessa aluna é ${lista[1][1]}`);
